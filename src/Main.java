211ca6f68242b505d56fc867c3bb802e13eee55a
Felipealdana <jaldanam@unal.edu.co>
2025-02-27 20:38:13 -0500

    hola

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import Interfaz.Interfaz;
+
+public class Main {
+    public static void main(String[] args) {
+        Interfaz interfaz = new Interfaz();
+        interfaz.setVisible(true);
+    }
+}
